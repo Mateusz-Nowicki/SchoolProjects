a42581a58768ed03a643290fbf2a8d2cfba7f705
Mateusz-Nowicki <mateusz.nowicki2006@gmail.com>
2024-04-30 00:20:56 +0200

    feat(calculator): added surface calculation

--- Remonty/mainScript.js
+++ Remonty/mainScript.js
@@ -1,10 +1,10 @@
-function oblicze() {
+function Oblicz() {
   var wymiarx = wymiar1.value;
   var wymiary = wymiar2.value;
   
   var powierzchnia = Math.round(10*(wymiarx*2+wymiary*2)*2.7)/10;
   var koszt = 8*powierzchnia;
   
-  document.getElementById("ak2").innerHTML = "Powierzchnai całkowita ścian:" +powierzchnia + "m2";
-  document.getElementById("ak3").innerHTML = "Koszt" +koszt + " zł";
+  document.getElementById("wynik1_js").innerHTML = "Powierzchnia całkowita ścian: " +powierzchnia + "m2";
+  document.getElementById("wynik2_js").innerHTML = "Koszt malowania: " +koszt + " zł";
   }
